002a22131b556f967eac2c50ec3dd5f41b49bbcd
bsnsone <ggiftcardone@gmail.com>
2025-09-03 03:27:20 +0600

    Create mega_env.js

---
+++ api/lib/mega_env.js
@@ -0,0 +1,9 @@
+// api/lib/mega_env.js
+
+const megaCredentials = {
+  email: "ggiftcardone@gmail.com",
+  password: "Shihab@Imran",
+  folderName: "test_api"
+};
+
+module.exports = megaCredentials;
